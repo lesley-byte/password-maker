bc750a87348b9b7de7e0484d089c858ace8dccf5
lesley-byte <phosphorescence3@hotmail.com>
2022-10-19 18:03:22 -0400

    Initial Local Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Assignment Code
+let generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
