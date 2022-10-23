5ac747759e000829d6bc1b3db43fceb121551e82
lesley-byte <phosphorescence3@hotmail.com>
2022-10-23 14:41:53 -0400

    adding more detailed comments to script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,15 +3,19 @@ var generateBtn = document.querySelector("#generate");
 var copyPasswordBtn = document.querySelector("#copyPasswordBtn");
 // ------------------------------------------------------------
 function askUser() {
+  //this function asks the user for input
   console.log("askUser() was called");
   var passwordLength = prompt(
+    //this asks the user for the length of the password
     "How many characters would you like your password to be? It must be between 8 and 128 characters."
   );
   while (passwordLength < 8 || passwordLength > 128) {
+    //this is a loop that will keep asking the user for a valid input
     var wantToExit = confirm(
       "You must enter a number between 8 and 128. Would you like to quit the program?"
     );
     if (wantToExit) {
+      //this is the exit option
       return "quit";
     }
     passwordLength = prompt(
@@ -19,22 +23,28 @@ function askUser() {
     );
   }
   var isLowerCase = confirm(
+    //this asks the user if they want lowercase characters
     "Would you like to include lowercase characters?(Ok=Yes, Cancel=No)"
   );
   var isUpperCase = confirm(
+    //this asks the user if they want uppercase characters
     "Would you like to include uppercase characters?(Ok=Yes, Cancel=No)"
   );
   var isNumeric = confirm(
+    //this asks the user if they want numeric characters
     "Would you like to include numeric characters?(Ok=Yes, Cancel=No)"
   );
   var isSpecial = confirm(
+    //this asks the user if they want special characters
     "Would you like to include special characters?(Ok=Yes, Cancel=No)"
   );
   while (!isLowerCase && !isUpperCase && !isNumeric && !isSpecial) {
+    //this is a loop that will keep asking the user for a valid input
     var wantToExit = confirm(
       "You must select at least one character type. Would you like to quit the program?(Ok=Yes, Cancel=No)"
     );
     if (wantToExit) {
+      //this is the exit option
       return "quit";
     }
     alert("You must select at least one character type.");
@@ -53,33 +63,36 @@ function askUser() {
   }
   console.log("askUser() is returning");
   console.log(passwordLength, isLowerCase, isUpperCase, isNumeric, isSpecial);
-  return [passwordLength, isLowerCase, isUpperCase, isNumeric, isSpecial];
+  return [passwordLength, isLowerCase, isUpperCase, isNumeric, isSpecial]; //this returns the user input to the generatePassword() function
 }
 // ------------------------------------------------------------
 function generatePassword(choicyChoice) {
+  //this function generates the password
   if (choicyChoice === "quit") {
-    return "quit";
+    return "quit"; //this is the exit option
   }
-  var passwordLength = choicyChoice[0];
-  var isLowerCase = choicyChoice[1];
-  var isUpperCase = choicyChoice[2];
-  var isNumeric = choicyChoice[3];
-  var isSpecial = choicyChoice[4];
+  var passwordLength = choicyChoice[0]; //this is the length of the password
+  var isLowerCase = choicyChoice[1]; //this is the lowercase option
+  var isUpperCase = choicyChoice[2]; //this is the uppercase option
+  var isNumeric = choicyChoice[3]; //this is the numeric option
+  var isSpecial = choicyChoice[4]; //this is the special option
   console.log("generatePassword() was called");
   //declaring and testing ascii values for nuance characters
-  var bubb1 = String.fromCharCode(34);
+  var bubb1 = String.fromCharCode(34); //this is the double quote character
   console.log(bubb1); // "
-  var bubb2 = String.fromCharCode(92);
+  var bubb2 = String.fromCharCode(92); //this is the backslash character
   console.log(bubb2); // "
   var possibleCharacters =
-    ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&'()*+,-./:;<=>?@[]^_`{|}~" + bubb1 + bubb2);
-  var chosenCharacters = "";
-  var upperCaseChoice = possibleCharacters.slice(0, 26);
-  var lowerCaseChoice = possibleCharacters.slice(26, 52);
-  var numericChoice = possibleCharacters.slice(52, 62);
-  var specialChoice = possibleCharacters.slice(62, 96);
-  var chosenCharacters = "";
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&'()*+,-./:;<=>?@[]^_`{|}~" +
+    bubb1 +
+    bubb2; //this is the string of all possible characters
+  var chosenCharacters = ""; //this is the string of characters that the user wants
+  var upperCaseChoice = possibleCharacters.slice(0, 26); //this is the string of uppercase characters
+  var lowerCaseChoice = possibleCharacters.slice(26, 52); //this is the string of lowercase characters
+  var numericChoice = possibleCharacters.slice(52, 62); //this is the string of numeric characters
+  var specialChoice = possibleCharacters.slice(62, 96); //this is the string of special characters
   if (isLowerCase && isUpperCase && isNumeric && isSpecial) {
+    //this is the option for all character types
     chosenCharacters = possibleCharacters;
     console.log(
       "Ok this should be all possible characters. chosenCharacters = " +
@@ -87,6 +100,7 @@ function generatePassword(choicyChoice) {
     );
   } else {
     if (isLowerCase) {
+      //this is the option for lowercase characters
       chosenCharacters = chosenCharacters.concat("", lowerCaseChoice);
       console.log(
         "This should be adding lowercase characters chosenCharacters = " +
@@ -94,6 +108,7 @@ function generatePassword(choicyChoice) {
       );
     }
     if (isUpperCase) {
+      //this is the option for uppercase characters
       chosenCharacters = chosenCharacters.concat("", upperCaseChoice);
       console.log(
         "This should be adding uppercase characters. chosenCharacters = " +
@@ -101,12 +116,14 @@ function generatePassword(choicyChoice) {
       );
     }
     if (isNumeric) {
+      //this is the option for numeric characters
       chosenCharacters = chosenCharacters.concat("", numericChoice);
       console.log(
         "This should be adding chosenCharacters = " + chosenCharacters
       );
     }
     if (isSpecial) {
+      //this is the option for special characters
       chosenCharacters = chosenCharacters.concat("", specialChoice);
       console.log(
         "This should be adding chosenCharacters = " + chosenCharacters
@@ -114,8 +131,9 @@ function generatePassword(choicyChoice) {
     }
   }
   console.log("chosenCharacters = " + chosenCharacters);
-  var passwordy = "";
+  var passwordy = ""; //this is the string that will be returned to the writePassword() function
   for (var i = 0; i < passwordLength; i++) {
+    //this is the loop that generates the password
     passwordy =
       passwordy +
       chosenCharacters.charAt(
@@ -124,48 +142,51 @@ function generatePassword(choicyChoice) {
   }
   console.log("generatePassword() is returning");
   console.log(passwordy);
-  return passwordy;
+  return passwordy; //this returns the password to the writePassword() function
 }
 // ------------------------------------------------------------
 function writePassword() {
+  //this function writes the password to the page
   console.log("writePassword() was called");
   var password = "Your Secure Password is: " + generatePassword(askUser());
   if (password === "Your Secure Password is: quit") {
+    //this is the exit option
     password = "You quit the program.";
-    var advicey = "You quit the program. Click the button to try again.";
+    var advicey = "You quit the program. Click the button to try again."; //this is the advice that will be displayed
     var passwordText = document.querySelector("#password");
     var advisement = document.querySelector("#isGenerated");
     passwordText.value = password;
     advisement.value = advicey;
-    alert("***BeeBoop!*** You quit the program. Click the red button to try again!");
+    alert(
+      "***BeeBoop!*** You quit the program. Click the red button to try again!"
+    ); //this is the alert that will be displayed if the user quits
     console.log("you quit the program");
   } else {
-  var advicey = "Please copy your password to a safe place.";
-  var passwordText = document.querySelector("#password");
-  var advisement = document.querySelector("#isGenerated");
-  passwordText.value = password;
-  advisement.value = advicey;
+    var advicey = "Please copy your password to a safe place."; //this is the advice that will be displayed
+    var passwordText = document.querySelector("#password");
+    var advisement = document.querySelector("#isGenerated");
+    passwordText.value = password;
+    advisement.value = advicey;
   }
 }
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); //this is the event listener that calls the writePassword() function when the button is clicked
 
 // function copyPassword() {
-  document.getElementById("copyPasswordBtn")
-  .onclick = function() {
+document.getElementById("copyPasswordBtn").onclick = //this is the event listener that calls the copyPassword() function when the button is clicked
+  function () {
     let text = document.getElementById("password").value;
     var text2 = text.slice(25);
     console.log(text2);
-    navigator.clipboard.writeText(text2)
+    navigator.clipboard
+      .writeText(text2) //this is the function that copies the password to the clipboard
       .then(() => {
-        alert(text2 + ' copied to clipboard');
+        alert(text2 + " copied to clipboard"); //this is the alert that will be displayed if the user copies the password
       })
-      .catch(err => {
-        alert('Error in copying text: ', err);
+      .catch((err) => {
+        alert("Error in copying text: ", err); //this is the alert that will be displayed if the user fails to copy the password
       });
-  }
+  };
 // }
 
 // copyPasswordBtn.addEventListener("click", copyPassword);
-
-
